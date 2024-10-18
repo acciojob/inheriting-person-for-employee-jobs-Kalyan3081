78cc3108228f35608008e08492888eb9540aa18f
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-10-18 17:37:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ Employee.prototype.constructor = Employee;
 
 // Add jobGreet method to the Employee prototype
 Employee.prototype.jobGreet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    // console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
 // Do not change code below this line
